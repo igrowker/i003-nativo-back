c0302f7cf7c443b9106c01203ae47f79ec35c794
Damián Fanaro <damianfanaro@gmail.com>
2024-09-08 14:22:12 +0200

    Initial commit

---
+++ src/main/java/com/igrowker/miniproject/rest/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.igrowker.miniproject.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE countries (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO countries (id, name) VALUES (1, 'USA');
+INSERT INTO countries (id, name) VALUES (2, 'France');
+INSERT INTO countries (id, name) VALUES (3, 'Brazil');
+INSERT INTO countries (id, name) VALUES (4, 'Italy');
+INSERT INTO countries (id, name) VALUES (5, 'Canada');
