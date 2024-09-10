f8e5c1f62190ee94d501dcbd250686fcead9d903
MircoSantoni <santonimircoariel@gmail.com>
2024-09-10 13:51:37 -0300

    Hotfix Transaction
    Hotfix on Transaction and some Geters and Seters

--- src/main/java/com/igrowker/miniproject/entities/Account.java
+++ src/main/java/com/igrowker/miniproject/entities/Account.java
@@ -10,4 +10,30 @@ public class Account {
     private Long amount;
 
     private boolean enabled;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    
 }
--- src/main/java/com/igrowker/miniproject/entities/Transaction.java
+++ src/main/java/com/igrowker/miniproject/entities/Transaction.java
@@ -11,8 +11,69 @@ public class Transaction {
 
     private String receiver;
 
+    private Long amount;
+
     private TransactionType transactionType;
 
     private TransactionStatus transactionStatus;
 
+    private boolean enabled;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public TransactionStatus getTransactionStatus() {
+        return transactionStatus;
+    }
+
+    public void setTransactionStatus(TransactionStatus transactionStatus) {
+        this.transactionStatus = transactionStatus;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    
 }
