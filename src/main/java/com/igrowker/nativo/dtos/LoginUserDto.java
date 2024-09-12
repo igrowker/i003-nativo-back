72caa32c3faad47ee03e875c82070b48e591a919
MircoSantoni <santonimircoariel@gmail.com>
2024-09-12 10:31:58 -0300

    Initial Dtos for security
    Dtos for User, LoginUser and RegisterUser

---
+++ src/main/java/com/igrowker/nativo/dtos/LoginUserDto.java
@@ -0,0 +1,11 @@
+package com.igrowker.nativo.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record LoginUserDto (
+        @NotBlank
+        String password,
+        @NotNull
+        String email
+    ) {}
---
+++ src/main/java/com/igrowker/nativo/dtos/UserDto.java
@@ -0,0 +1,11 @@
+package com.igrowker.nativo.dtos;
+
+public record UserDto (
+
+    Long dni,
+    String name,
+    String surname,
+    String email,
+    String phone
+
+) {}
