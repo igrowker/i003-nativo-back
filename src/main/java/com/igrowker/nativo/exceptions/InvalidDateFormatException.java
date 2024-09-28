3168ae1bea61df37d7f5889f89df839c73af45c0
mdev <matias.mazzitelli.dev@gmail.com>
2024-09-28 12:26:43 -0300

    Update/refactor Payment Controller and Service, Create DateFormatter (for global use) in Utils, Update Validations, add InvalidDateFormatException and QrGenerationException, modified GlobalExceptionHandler

---
+++ src/main/java/com/igrowker/nativo/exceptions/InvalidDateFormatException.java
@@ -0,0 +1,7 @@
+package com.igrowker.nativo.exceptions;
+
+public class InvalidDateFormatException extends RuntimeException{
+    public InvalidDateFormatException(String message) {
+        super(message);
+    }
+}
